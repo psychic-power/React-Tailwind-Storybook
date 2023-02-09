ca8e460ecef6732bb1ccd6d25ff78da11cc437a6
PsychicPower <psycpv@gmail.com>
2023-02-09 15:40:11 +0800

    Update

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,12 +1,25 @@
 export const ButtonType = {
-  primary: "middle none center mr-4 rounded-lg bg-purple-500 font-sans text-xs font-bold uppercase text-white shadow-md shadow-purple-500/20 transition-all hover:shadow-lg hover:shadow-purple-500/40 active:opacity-[0.85] active:shadow-none",
-  secondary: "middle none center mr-4 rounded-lg bg-blue-500 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 active:opacity-[0.85] active:shadow-none",
-  basic: "middle none center mr-4 rounded-lg bg-gray-500 font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-500/20 transition-all hover:shadow-lg hover:shadow-gray-500/40 active:opacity-[0.85] active:shadow-none",
-  danger: "middle none center mr-4 rounded-lg bg-red-500 font-sans text-xs font-bold uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 active:opacity-[0.85] active:shadow-none",
+  primary:
+    "middle none center mr-4 rounded-lg bg-purple-500 font-bold uppercase text-white shadow-md shadow-purple-500/20 transition-all hover:shadow-lg hover:shadow-purple-500/40 active:opacity-[0.85] active:shadow-none",
+  secondary:
+    "middle none center mr-4 rounded-lg bg-blue-500 font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 active:opacity-[0.85] active:shadow-none",
+  basic:
+    "middle none center mr-4 rounded-lg bg-gray-500 font-bold uppercase text-white shadow-md shadow-gray-500/20 transition-all hover:shadow-lg hover:shadow-gray-500/40 active:opacity-[0.85] active:shadow-none",
+  danger:
+    "middle none center mr-4 rounded-lg bg-red-500 font-bold uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 active:opacity-[0.85] active:shadow-none",
 };
 
 export const ButtonSize = {
   sm: "py-2 px-4 text-xs",
   md: "py-2.5 px-5 text-base",
   lg: "py-3 px-6 text-xl",
+};
+
+export const FontFamily = {
+  Default: "font-sans",
+  Bodoni: "font-bodoni",
+  Garamond: "font-garamond",
+  Helvetica: "font-helvetica",
+  Verdana: "font-verdana",
+  Poppins: "font-poppins",
 };
