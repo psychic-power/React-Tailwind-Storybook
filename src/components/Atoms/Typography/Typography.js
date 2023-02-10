6879147453316062f7fb0a6a55c83996f138e410
PsychicPower <psycpv@gmail.com>
2023-02-11 04:41:01 +0800

    Atom typography

---
+++ src/components/Atoms/Typography/Typography.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { TextColor } from "../../../utils/theme";
+
+export default function Typography({ type, children, color }) {
+  switch (type) {
+    case "h1":
+      return (
+        <h1
+          className={`text-h1 font-semibold ${
+            TextColor[color ? color : "primary"]
+          }`}
+        >
+          {children}
+        </h1>
+      );
+    case "h2":
+      return (
+        <h2
+          className={`text-h2 font-semibold ${
+            TextColor[color ? color : "primary"]
+          }`}
+        >
+          {children}
+        </h2>
+      );
+    case "h3":
+      return (
+        <h3
+          className={`text-h3 font-semibold ${
+            TextColor[color ? color : "primary"]
+          }`}
+        >
+          {children}
+        </h3>
+      );
+    case "p-semibold":
+      return (
+        <p
+          className={`text-p font-semibold ${
+            TextColor[color ? color : "primary"]
+          }`}
+        >
+          {children}
+        </p>
+      );
+    case "p-medium":
+      return (
+        <p
+          className={`text-p font-medium ${TextColor[color ? color : "primary"]}`}
+        >
+          {children}
+        </p>
+      );
+    case "p-regular":
+      return (
+        <p
+          className={`text-p font-normal ${TextColor[color ? color : "primary"]}`}
+        >
+          {children}
+        </p>
+      );
+    case "caption":
+      return (
+        <p
+          className={`text-caption font-normal ${
+            TextColor[color ? color : "primary"]
+          }`}
+        >
+          {children}
+        </p>
+      );
+    case "caption2":
+      return (
+        <p
+          className={`text-caption2 font-normal ${
+            TextColor[color ? color : "primary"]
+          }`}
+        >
+          {children}
+        </p>
+      );
+    default:
+      return (
+        <p
+          className={`text-p font-normal ${TextColor[color ? color : "primary"]}`}
+        >
+          {children}
+        </p>
+      );
+  }
+}
---
+++ src/components/Atoms/Typography/Typography.stories.js.js
@@ -0,0 +1,105 @@
+import Typography from "./Typography";
+
+export default {
+  title: "Components/UI-Base Elements/Typography",
+  component: Typography,
+  argTypes: {
+    type: {
+      control: {
+        type: "select",
+      },
+      options: [
+        "h1",
+        "h2",
+        "h3",
+        "p-semibold",
+        "p-medium",
+        "p-regular",
+        "caption",
+        "caption2",
+      ],
+    },
+    color: {
+      control: {
+        type: "select",
+      },
+      options: ["primary", "secondary", "tetriary"],
+    },
+  },
+};
+
+const Template = (args) => <Typography {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children:
+    "Felis sit ac pretium viverra in. Ut purus mattis tempor nullam lectus ultrices mi. Non vulputate nisl et quam a.",
+  type: "p-regular",
+  color: "primary",
+};
+
+export const h1 = Template.bind({});
+h1.args = {
+  children:
+    "Felis sit ac pretium viverra in. Ut purus mattis tempor nullam lectus ultrices mi. Non vulputate nisl et quam a.",
+  type: "h1",
+  color: "primary",
+};
+
+export const h2 = Template.bind({});
+h2.args = {
+  children:
+    "Felis sit ac pretium viverra in. Ut purus mattis tempor nullam lectus ultrices mi. Non vulputate nisl et quam a.",
+  type: "h2",
+  color: "primary",
+};
+
+export const h3 = Template.bind({});
+h3.args = {
+  children:
+    "Felis sit ac pretium viverra in. Ut purus mattis tempor nullam lectus ultrices mi. Non vulputate nisl et quam a.",
+  type: "h3",
+  color: "primary",
+};
+
+export const pSemibold = Template.bind({});
+pSemibold.args = {
+  children:
+    "Felis sit ac pretium viverra in. Ut purus mattis tempor nullam lectus ultrices mi. Non vulputate nisl et quam a.",
+  type: "p-semibold",
+  color: "primary",
+};
+
+export const pMedium = Template.bind({});
+pMedium.args = {
+  children:
+    "Felis sit ac pretium viverra in. Ut purus mattis tempor nullam lectus ultrices mi. Non vulputate nisl et quam a.",
+  type: "p-medium",
+  color: "primary",
+};
+
+export const pRegular = Template.bind({});
+pRegular.args = {
+  children:
+    "Felis sit ac pretium viverra in. Ut purus mattis tempor nullam lectus ultrices mi. Non vulputate nisl et quam a.",
+  type: "p-regular",
+  color: "primary",
+};
+
+export const caption = Template.bind({});
+caption.args = {
+  children:
+    "Felis sit ac pretium viverra in. Ut purus mattis tempor nullam lectus ultrices mi. Non vulputate nisl et quam a.",
+  type: "caption",
+  color: "primary",
+};
+
+export const caption2 = Template.bind({});
+caption2.args = {
+  children:
+    "Felis sit ac pretium viverra in. Ut purus mattis tempor nullam lectus ultrices mi. Non vulputate nisl et quam a.",
+  type: "caption2",
+  color: "primary",
+};
+
+
