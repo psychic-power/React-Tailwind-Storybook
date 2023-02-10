34d44651d253450c604e89df637705ae8c470f25
PsychicPower <psycpv@gmail.com>
2023-02-10 15:30:03 +0800

    Input Font and Colors

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -22,6 +22,7 @@ export const PaginateColor = {
 }
 
 export const FontFamily = {
+  Inter: "font-inter",
   Default: "font-sans",
   Bodoni: "font-bodoni",
   Garamond: "font-garamond",
@@ -29,3 +30,15 @@ export const FontFamily = {
   Verdana: "font-verdana",
   Poppins: "font-poppins",
 };
+
+const textColors = ["text-white", "text-black", 
+"text-gray-50", "text-gray-100", "text-gray-200", "text-gray-300", "text-gray-400", "text-gray-500", "text-gray-600", "text-gray-700", "text-gray-800", "text-gray-900",
+"text-red-50", "text-red-100", "text-red-200", "text-red-300", "text-red-400", "text-red-500", "text-red-600", "text-red-700", "text-red-800", "text-red-900",
+"text-yellow-50", "text-yellow-100", "text-yellow-200", "text-yellow-300", "text-yellow-400", "text-yellow-500", "text-yellow-600", "text-yellow-700", "text-yellow-800", "text-yellow-900",
+"text-teal-50", "text-teal-100", "text-teal-200", "text-teal-300", "text-teal-400", "text-teal-500", "text-teal-600", "text-teal-700", "text-teal-800", "text-teal-900",
+"text-blue-50", "text-blue-100", "text-blue-200", "text-blue-300", "text-blue-400", "text-blue-500", "text-blue-600", "text-blue-700", "text-blue-800", "text-blue-900",
+"text-indigo-50", "text-indigo-100", "text-indigo-200", "text-indigo-300", "text-indigo-400", "text-indigo-500", "text-indigo-600", "text-indigo-700", "text-indigo-800", "text-indigo-900",
+"text-purple-50", "text-purple-100", "text-purple-200", "text-purple-300", "text-purple-400", "text-purple-500", "text-purple-600", "text-purple-700", "text-purple-800", "text-purple-900",
+"text-pink-50", "text-pink-100", "text-pink-200", "text-pink-300", "text-pink-400", "text-pink-500", "text-pink-600", "text-pink-700", "text-pink-800", "text-pink-900",
+"text-social-facebook", "text-social-twitter", "text-social-google", "text-social-dribble", "text-social-github",
+]
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,22 @@ module.exports = {
         'garamond': ['Garamond', 'Garamond'],
         'helvetica': ['Helvetica', 'Helvetica'],
         'verdana': ['Verdana', 'Verdana'],
+        'inter': ['Inter', 'Inter'],
+      },
+      fontSize: {
+        h1: ['22px', '24px'],
+        h2: ['19px', '22px'],
+        h3: ['16px', '20px'],
+        p: ['13px', '18px'],
+        caption: ['11px', '16px'],
+        caption2: ['9px', '16px'],
+      },
+      textColor: {
+        "social-facebook": "#35518D",
+        "social-twitter": "#1DA1F2;",
+        "social-google": "#4284F4;",
+        "social-dribble": "#EA4C89",
+        "social-github":  "#1B1F23",
       }
     },
   },
