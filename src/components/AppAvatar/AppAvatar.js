b8d975478838e92e80b23bfbc5fe1d96ed645edd
PsychicPower <psycpv@gmail.com>
2023-02-11 00:24:51 +0800

    AppProfile button

--- src/components/AppAvatar/AppAvatar.js
+++ src/components/AppAvatar/AppAvatar.js
@@ -3,19 +3,19 @@ import AppTypography from "../AppTypography/AppTypography";
 
 export default function AppAvatar({ username, image, size }) {
   const nameSplit = username?.split(" ");
-  const nameAvatar = nameSplit.map(item => item[0]);
+  const nameAvatar = nameSplit?.map(item => item[0]);
   console.log("nameAvatar => ", nameAvatar);
 
   return (
     <div className={`rounded-full`}>
       {image ? (
-        <img src={image} className={`rounded-full w-${size} h-${size} ring-2 `}/>
+        <img src={image} className={`rounded-full w-${size} h-${size} `}/>
       ) : username ? (
         <div className={`rounded-full flex justify-center items-center text-gray-50 bg-gray-800 w-${size} h-${size}`}>
           <AppTypography colorClassName="text-gray-50">{nameAvatar}</AppTypography>
         </div>
       ) : (
-        <div className="rounded-full w-fit bg-gray-300">
+        <div className="rounded-full w-fit p-1 bg-gray-300">
           <UserIcon className={`w-${size} h-${size} text-gray-800`} />
         </div>
       )}
