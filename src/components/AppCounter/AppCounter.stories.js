34ef9eaca3f03c33a2aab68e82fd7d68cfa8a1ce
PsychicPower <psycpv@gmail.com>
2023-02-11 01:54:16 +0800

    AppCounter default stories change

--- src/components/AppCounter/AppCounter.stories.js
+++ src/components/AppCounter/AppCounter.stories.js
@@ -9,7 +9,7 @@ const Template = args => <AppCounter {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  children: "Test",
+  children: 1,
   textClassName: "text-gray-900 dark:text-gray-50",
   bgClassName: "bg-gray-100 dark:bg-gray-800",
 };
