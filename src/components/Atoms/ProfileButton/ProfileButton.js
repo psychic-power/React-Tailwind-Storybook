aeb0002164338bb619d7437974a4ae17b49b245e
PsychicPower <psycpv@gmail.com>
2023-02-11 16:50:39 +0800

    Profile Button Component

---
+++ src/components/Atoms/ProfileButton/ProfileButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Avatar from "../Avatar/Avatar";
+
+export default function ProfileButton({ image, username, size }) {
+  return (
+    <button
+      className={`flex justify-center items-center p-2 rounded w-fit bg-surface-base-light hover:bg-action-hover-light focus:bg-action-select-light dark:bg-surface-base-dark dark:hover:bg-action-hover-dark dark:focus:bg-action-select-dark focus:ring-gray-800 dark:focus:ring-gray-100 focus:ring-2`}
+    >
+      <Avatar size={size} image={image} username={username}/>
+    </button>
+  );
+}
