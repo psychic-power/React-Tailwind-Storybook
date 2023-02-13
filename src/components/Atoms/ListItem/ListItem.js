562a8ef71511095b7556c0574997fa68fdf45999
PsychicPower <psycpv@gmail.com>
2023-02-13 22:06:26 +0800

    List Item components

---
+++ src/components/Atoms/ListItem/ListItem.js
@@ -0,0 +1,18 @@
+import CircleProgress from "../CircularProgressBar/CircleProgress";
+import CustomIcon from "../CustomIcon/CustomIcon";
+import Typography from "../Typography/Typography"
+
+export default function ListItem({itemname, iconname, percent}) {
+  return (
+    <div className="w-40 h-8 flex justify-between rounded cursor-pointer items-center pl-3.5 pr-2 bg-surface-base-light hover:bg-action-hover-light active:bg-action-select-light dark:bg-surface-base-dark dark:hover:bg-action-hover-dark dark:active:bg-action-select-dark">
+      <div className="flex space-x-2 items-center">
+        <CustomIcon icon={iconname} size="14" />
+        <Typography type="pMedium">{itemname}</Typography>
+      </div>
+      <div className="flex space-x-1 items-center">
+        <Typography type="caption">{percent}%</Typography>
+        <CircleProgress percentage={percent} />
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Atoms/ListItem/ListItem.stories.js
@@ -0,0 +1,14 @@
+import ListItem from "./ListItem";
+
+export default {
+  title:"Components/Atoms/ListItem",
+  component: ListItem,
+}
+
+const Template = (args) => <ListItem {...args} />
+
+export const Default = Template.bind({});
+Default.args = {
+  itemname: "Item",
+  percent: 100 
+}
