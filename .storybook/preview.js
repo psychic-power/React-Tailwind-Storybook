a294d3d70bba8241eb7fc30e7efb02509973e162
PsychicPower <psycpv@gmail.com>
2023-02-13 20:28:20 +0800

    Add preview background for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -10,15 +10,31 @@ export const parameters = {
     },
   },
   backgrounds: {
-    default: 'light',
+    default: 'Surface-Light-Base',
     values: [
       {
-        name: 'light',
+        name: 'Surface-Light-Base',
         value: '#F9FAFB;',
       },
       {
-        name: 'dark',
+        name: 'Surface-Light-Overlay',
+        value: '#FFFFFF;',
+      },
+      {
+        name: 'Surface-Light-Alt',
+        value: '#E5E7EB;',
+      },
+      {
+        name: 'Surface-Dark-Base',
         value: '#0E121A;',
+      },
+      {
+        name: 'Surface-Dark-Overlay',
+        value: '#111928;',
+      },
+      {
+        name: 'Surface-Dark-Alt',
+        value: '#1F2A37;',
       },
     ],
   },
@@ -38,19 +54,6 @@ export const globalTypes = {
       // Change title based on selected value
       dynamicTitle: true,
     },
-  },
-  backgrounds: {
-    default: 'light',
-    values: [
-      {
-        name: 'light',
-        value: '#00aced',
-      },
-      {
-        name: 'dark',
-        value: '#3b5998',
-      },
-    ],
   },
 };
 
