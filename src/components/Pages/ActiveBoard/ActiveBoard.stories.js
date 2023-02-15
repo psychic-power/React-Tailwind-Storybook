416bbcf04691e58e78d5fc2a6860f1fc7075a7ee
PsychicPower <psycpv@gmail.com>
2023-02-15 10:06:36 +0800

    Active board page

---
+++ src/components/Pages/ActiveBoard/ActiveBoard.stories.js
@@ -0,0 +1,14 @@
+import ActiveBoard from "./ActiveBoard";
+
+export default {
+  title: 'Components/Pages/ActiveBoard',
+  component: ActiveBoard,
+}
+
+const Template = args => <ActiveBoard {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  // abbr: true,
+};
+
