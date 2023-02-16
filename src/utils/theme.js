d70db4120fae0cdf1f022a50fe65cbfb3ae254e3
PsychicPower <psycpv@gmail.com>
2023-02-16 12:54:16 +0800

    Update color and Typography.

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,52 +1,23 @@
-export const ButtonType = {
-  primary:
-    "middle none center mr-4 rounded-lg bg-purple-500 font-bold uppercase text-white shadow-md shadow-purple-500/20 transition-all hover:shadow-lg hover:shadow-purple-500/40 active:opacity-[0.85] active:shadow-none",
-  secondary:
-    "middle none center mr-4 rounded-lg bg-blue-500 font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 active:opacity-[0.85] active:shadow-none",
-  basic:
-    "middle none center mr-4 rounded-lg bg-gray-500 font-bold uppercase text-white shadow-md shadow-gray-500/20 transition-all hover:shadow-lg hover:shadow-gray-500/40 active:opacity-[0.85] active:shadow-none",
-  danger:
-    "middle none center mr-4 rounded-lg bg-red-500 font-bold uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 active:opacity-[0.85] active:shadow-none",
-};
-
-export const ButtonSize = {
-  sm: "py-2 px-4 text-xs",
-  md: "py-2.5 px-5 text-base",
-  lg: "py-3 px-6 text-xl",
-};
-
-export const PaginateColor = {
-  basic: "bg-gray-200 hover:bg-gray-300",
-  primary: "bg-purple-700 hover:bg-purple-900 text-white",
-  secondary: "bg-blue-700 hover:bg-blue-900 text-white",
+const Color = {
+  background: {
+    base:"bg-surface-base-light dark:bg-surface-base-dark",
+    overlay:"bg-surface-overlay-light dark:bg-surface-overlay-dark",
+    alt:"bg-surface-alt-light dark:bg-surface-alt-dark",
+    div:"bg-surface-div-light dark:bg-surface-base-dark"
+  },
+  action: {
+    hover: "hover:bg-action-hover-light dark:hover:bg-action-hover-dark",
+    selected: "focus:bg-action-hover-light dark:focus:bg-action-hover-dark"
+  },
+  text: {
+    primary: "text-content-primary-light dark:text-content-primary-dark",
+    secondary: "text-content-secondary-light dark:text-content-secondary-dark",
+    tetriary: "text-content-primary-light dark:text-content-primary-dark"
+  },
+  support: {
+    accent: "bg-accent-light dark:bg-accent-dark",
+    success: "bg-success-light dark:bg-success-dark",
+    error: "bg-error-light dark:bg-error-dark",
+    progress: "bg-progress-light dark:bg-progress-dark"
+  }
 }
-
-export const FontFamily = {
-  Inter: "font-inter",
-  Default: "font-sans",
-  Bodoni: "font-bodoni",
-  Garamond: "font-garamond",
-  Helvetica: "font-helvetica",
-  Verdana: "font-verdana",
-  Poppins: "font-poppins",
-};
-
-export const TextColor = {
-  primary: "text-content-primary-light dark:text-content-primary-dark",
-  secondary: "text-content-secondary-light dark:text-content-secondary-dark",
-  tetriary: "text-content-tetriary-light dark:text-content-tetriary-dark",
-  select: "text-action-select-light dark:text-action-select-dark",
-  hover: "text-action-hover-light dark:text-action-hover-dark",
-  primaryDark:"text-content-primary-dark",
-}
-
-export const BgColor = {
-  primary: "bg-content-primary-light dark:bg-content-primary-dark",
-  secondary: "bg-content-secondary-light dark:bg-content-secondary-dark",
-  tetriary: "bg-content-tetriary-light dark:bg-content-tetriary-dark",
-  select: "bg-action-select-light dark:bg-action-select-dark",
-  hover: "bg-action-hover-light dark:bg-action-hover-dark",
-  altDark: "bg-surface-alt-dark",
-}
-
-
